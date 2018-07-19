76f2881e49017257b2802f15fbb36bc6f2d51665
Alexey Minchakov <lexaaim@gmail.com>
2018-07-19 15:26:43 +0300

    add is_exists_and_locked func

--- difffunc.py
+++ difffunc.py
@@ -6,22 +6,40 @@ import sys
 import subprocess
 import tempfile
 
-def temporary_file( filetemplate='sample' ):
-    'Генерирует уникальное для временного каталога имя файла по переданному шаблону. Возвращает путь к файлу'
-    cnt = 1
-    while True:
-        newfilename = '{0:s}{1:s}{2:s}{3:0>2d}.xlsx'.format( tempfile.gettempdir(), os.sep, filetemplate, cnt )
-        if os.path.isfile(newfilename):
-            cnt+=1
-        else:
+def is_exists_and_locked(filepath):
+    """Checks if a file is locked by opening it in append mode.
+    If no exception thrown, then the file is not locked.
+    """
+    if not os.path.exists(filepath):
+        return False
+
+    file_object = None
+    try:
+        file_object = open(filepath, 'a')
+        if file_object:
+            file_object.close()
+            return False
+
+    except BaseException:
+        return True
+
+def temporary_file( nametemplate='sample' ):
+    """Generates the unique name of file for given template
+    in system user termporary directory'
+    """
+    for cnt in range( 1, 100 ):
+        newfilename = '{0:s}{1:s}{2:s}{3:0>2d}.xlsx'.format( \
+                tempfile.gettempdir(), os.sep, nametemplate, cnt )
+        if not is_exists_and_locked( newfilename ):
             break
 
     return newfilename
 
 def open_file( filename ):
-    'Opens file in app which is associated with file\'s extension'
+    """Opens file in app which is associated with file's extension
+    """
     if sys.platform == 'win32':
         os.startfile( filename )
     else:
         opener = 'open' if sys.platform == 'darwin' else 'xdg-open'
-        subprocess.call( [opener, filename] )
+        os.system( "{0:s} {1:s} &".format( opener, filename ) )
