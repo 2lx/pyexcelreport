5e10b959a9359978cdf6a90b70f116940a7793e4
Alexey Minchakov <lexaaim@gmail.com>
2018-07-21 22:54:48 +0300

    fixed platform dependencies

--- sqltabledata.py
+++ sqltabledata.py
@@ -2,39 +2,46 @@
 # -*- coding: utf-8 -*-
 
 from xlstable import *
-import config
-import pymssql
 import re
 import datetime
 
-UUID_PATTERN = re.compile(r'^[{]?([\da-f]{8}-([\da-f]{4}-){3}[\da-f]{12})[}]?$', re.IGNORECASE)
+import sys
+
 
 def maybe_sqlquoted(param):
+    """Возвращает переданный параметр окруженный кавычками (если это необходимо в SQL)
+    """
+    UUID_PATTERN = re.compile(r'^[{]?([\da-f]{8}-([\da-f]{4}-){3}[\da-f]{12})[}]?$', re.IGNORECASE)
     matched = UUID_PATTERN.match(param)
     if matched:
         param = "'{{{0:s}}}'".format(matched.group(1))
-#    if type(param) == [datetime.date]
+    elif type(param) == [datetime.date]:
+        param = "'{0:s}'".format(param.strftime('%d/%m/%Y %H:%M:%S.%f'))
     elif isinstance(param, str):
         param = "'{0:s}'".format(param)
     return param
 
 
-def get_table_data(sqlquery, table_info):
-    conn = pymssql.connect( server=config.mssql_server,
-                            user=config.db_login,
-                            password=config.db_password,
-                            database=config.db_catalog,
-                            autocommit=True)
-    cursor = conn.cursor(as_dict=True)
+if sys.platform.startswith('win'):
+    import config
+    import pymssql
+    def get_mssql_data(sqlquery, table_info):
+        conn = pymssql.connect( server=config.mssql_server,
+                                user=config.db_login,
+                                password=config.db_password,
+                                database=config.db_catalog,
+                                autocommit=True)
+        cursor = conn.cursor(as_dict=True)
 
-    cursor.execute(sqlquery)
-    table_data = []
+        cursor.execute(sqlquery)
+        table_data = []
 
-    for row in cursor:
-        row_data = ()
-        for ti in table_info:
-            row_data += (row[ti.fieldname],) if ti.fieldname != '' else ('',)
-        table_data.append(row_data)
+        for row in cursor:
+            row_data = ()
+            for ti in table_info:
+                row_data += (row[ti.fieldname],) if ti.fieldname != '' else ('',)
+            table_data.append(row_data)
 
-    conn.close
-    return table_data
+        conn.close
+        return table_data
+
--- transport_TransferOfSamplesAct.py
+++ transport_TransferOfSamplesAct.py
@@ -8,6 +8,8 @@ from xlstable import *
 from sqltabledata import *
 
 import locale
+import sys
+
 locale.setlocale(locale.LC_ALL, 'ru_RU.UTF-8')
 
 THC=XLSTableHeaderColumn
@@ -48,18 +50,20 @@ table_info = (\
         TCI('',      'string', 1),
         )
 
-sqlprocedure = "ORDERS.dbo.OMS_TRANSPORT_ReportPackageGlobalInvoiceList"
-sqlparamlist = ( "{53DAD87F-8C0F-4178-9A27-9F686E44A8FD}", )
-sqlquery = "EXEC {0:s} {1:s}".format(sqlprocedure, ", ".join(map(maybe_sqlquoted, sqlparamlist)))
-#print(sqlquery)
-table_data = get_table_data( sqlquery, table_info )
-#table_data = ( \
-#        [ 'MSH05435', 'черный', 50, 0, 150, 100, 200, 0, 200, 0, 0, 0, 0, 0, 0, '1-50'  ],
-#        [ 'MSH05435', 'белый',   0, 0, 150,   0, 220, 0,   0, 0, 0, 0, 0, 0, 0, '1-50'  ],
-#        [ 'MSH05436', 'черный', 50, 0, 150, 200, 268, 0,   0, 0, 0, 0, 0, 0, 0, '31-50' ],
-#        [ 'MSH05437', 'черный',  0, 0, 150, 100, 205, 0, 200, 0, 0, 0, 0, 0, 0, '1-50'  ],
-#        [ 'MSH05437', 'красный',50, 0, 150, 100, 280, 0,   0, 0, 0, 0, 0, 0, 0, '1-50'  ],
-#        [ 'MSH05437', 'желтый', 50, 0, 150, 200, 200, 0,   0, 0, 0, 0, 0, 0, 0, '1-50'  ] )
+if sys.platform.startswith('win'):
+    sqlprocedure = "ORDERS.dbo.OMS_TRANSPORT_ReportPackageGlobalInvoiceList"
+    sqlparamlist = ( "{53DAD87F-8C0F-4178-9A27-9F686E44A8FD}", )
+    sqlquery = "EXEC {0:s} {1:s}".format(sqlprocedure, ", ".join(map(maybe_sqlquoted, sqlparamlist)))
+    #  print(sqlquery)
+    table_data = get_table_data( sqlquery, table_info )
+else:
+    table_data = ( \
+        [ 'MSH05435', 'черный', 50, 0, 150, 100, 200, 0, 200, 0, 0, 0, 0, 0, 0, '1-50'  ],
+        [ 'MSH05435', 'белый',   0, 0, 150,   0, 220, 0,   0, 0, 0, 0, 0, 0, 0, '1-50'  ],
+        [ 'MSH05436', 'черный', 50, 0, 150, 200, 268, 0,   0, 0, 0, 0, 0, 0, 0, '31-50' ],
+        [ 'MSH05437', 'черный',  0, 0, 150, 100, 205, 0, 200, 0, 0, 0, 0, 0, 0, '1-50'  ],
+        [ 'MSH05437', 'красный',50, 0, 150, 100, 280, 0,   0, 0, 0, 0, 0, 0, 0, '1-50'  ],
+        [ 'MSH05437', 'желтый', 50, 0, 150, 200, 200, 0,   0, 0, 0, 0, 0, 0, 0, '1-50'  ] )
 
 table = XLSTable(table_info, table_data )
 cur_row = rep.apply_table(table, first_row=cur_row)
